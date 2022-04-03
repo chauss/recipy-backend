4e2a90a27580505c88067acd039f033e5598517d
Christopher Hauß <christopher_hauss@gmx.de>
2022-04-03 17:29:40 +0200

    added main test to local profile

--- src/test/kotlin/de/chauss/recipy/RecipyBackendApplicationTests.kt
+++ src/test/kotlin/de/chauss/recipy/RecipyBackendApplicationTests.kt
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Profile
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootTest
+@Profile("local")
 class RecipyBackendApplicationTests {
 
 	@Test
