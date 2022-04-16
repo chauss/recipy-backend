36e56ba2f9e329e537fdd16d5b6c0fe3cc6968fa
Christopher Hauß <christopher_hauss@gmx.de>
2022-04-16 22:07:28 +0200

    added new endpoint to get a recipy by id

--- src/main/kotlin/de/chauss/recipy/api/RecipeRestController.kt
+++ src/main/kotlin/de/chauss/recipy/api/RecipeRestController.kt
@@ -1,10 +1,13 @@
 package de.chauss.recipy.api
 
 import de.chauss.recipy.service.RecipeService
+import de.chauss.recipy.service.dtos.RecipeDto
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping(value = ["/v1"])
@@ -18,6 +21,15 @@ class RecipeRestController(
     fun createRecipe(@RequestBody request: CreateRecipeRequest): ResponseEntity<CreationResponse> {
         val result = recipeService.createRecipe(request.name)
         return CreationResponse.responseEntityForResult(result = result)
+    }
+
+    @GetMapping("/recipe/{recipeId}")
+    fun getRecipeById(@PathVariable(value = "recipeId") recipeId: String): RecipeDto {
+        val result = recipeService.getRecipeById(recipeId = recipeId)
+        if (result != null) {
+            return result
+        }
+        throw ResponseStatusException(HttpStatus.NOT_FOUND, "No recipe with the given id found")
     }
 }
 
