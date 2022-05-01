98f11df71e2212eb862f7068ba22adbd6ebfcf62
Christopher Hauß <christopher_hauss@gmx.de>
2022-05-01 19:52:01 +0200

    fixed duplicates in fetch

--- src/main/kotlin/de/chauss/recipy/database/models/Recipe.kt
+++ src/main/kotlin/de/chauss/recipy/database/models/Recipe.kt
@@ -1,6 +1,8 @@
 package de.chauss.recipy.database.models
 
 import jakarta.persistence.*
+import org.hibernate.annotations.Fetch
+import org.hibernate.annotations.FetchMode
 import org.springframework.data.jpa.repository.JpaRepository
 import java.time.Instant
 import java.util.*
@@ -12,9 +14,11 @@ class Recipe(
     @Column(unique = true)
     val name: String = "",
     @OneToMany(fetch = FetchType.EAGER, mappedBy = "recipe", cascade = [CascadeType.REMOVE])
+    @Fetch(FetchMode.SUBSELECT)
     val ingredientUsages: Set<IngredientUsage> = HashSet(),
     val created: Instant = Instant.now(),
     @OneToMany(fetch = FetchType.EAGER, mappedBy = "recipe", cascade = [CascadeType.REMOVE])
+    @Fetch(FetchMode.SUBSELECT)
     val preparationSteps: List<PreparationStep> = ArrayList(),
 )
 
