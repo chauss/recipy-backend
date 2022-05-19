bb9342874f88c0b0176b5b6da2af8864492f5a18
Christopher Hauß <christopher_hauss@gmx.de>
2022-05-19 20:14:23 +0200

    adds flyway database migration, updates some dependencies, adds initial db inserts

---
+++ src/main/resources/db/migration/V1__initial_ddl.sql
@@ -0,0 +1,85 @@
+CREATE SCHEMA recipy;
+
+-- INGREDIENT UNITS
+CREATE TABLE recipy.ingredient_units
+(
+    ingredient_unit_id character varying(255) NOT NULL,
+    created            timestamp without time zone,
+    name               character varying(255),
+    CONSTRAINT ingredient_units_pkey PRIMARY KEY (ingredient_unit_id),
+    CONSTRAINT ingredient_unit_name_unique UNIQUE (name)
+);
+
+ALTER TABLE recipy.ingredient_units
+    OWNER to root;
+
+-- INGREDIENTS
+CREATE TABLE recipy.ingredients
+(
+    ingredient_id character varying(255) NOT NULL,
+    created       timestamp without time zone,
+    name          character varying(255),
+    CONSTRAINT ingredients_pkey PRIMARY KEY (ingredient_id),
+    CONSTRAINT ingredients_name_unique UNIQUE (name)
+);
+
+ALTER TABLE recipy.ingredients
+    OWNER to root;
+
+-- RECIPE
+CREATE TABLE recipy.recipes
+(
+    recipe_id character varying(255) NOT NULL,
+    created   timestamp without time zone,
+    name      character varying(255),
+    CONSTRAINT recipes_pkey PRIMARY KEY (recipe_id),
+    CONSTRAINT recipes_name_unique UNIQUE (name)
+);
+
+ALTER TABLE recipy.recipes
+    OWNER to root;
+
+
+-- INGREDIENT USAGES
+CREATE TABLE recipy.ingredient_usages
+(
+    ingredient_usage_id character varying(255) NOT NULL,
+    amount              double precision       NOT NULL,
+    created             timestamp without time zone,
+    ingredient_id       character varying(255),
+    ingredient_unit_id  character varying(255),
+    recipe_id           character varying(255),
+    CONSTRAINT ingredient_usages_pkey PRIMARY KEY (ingredient_usage_id),
+    CONSTRAINT ingredient_usages_recipe_fkey FOREIGN KEY (recipe_id)
+        REFERENCES recipy.recipes (recipe_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT ingredient_usages_ingredient_unit_fkey FOREIGN KEY (ingredient_unit_id)
+        REFERENCES recipy.ingredient_units (ingredient_unit_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT ingredient_usages_ingredient_fkey FOREIGN KEY (ingredient_id)
+        REFERENCES recipy.ingredients (ingredient_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+);
+
+ALTER TABLE recipy.ingredient_usages
+    OWNER to root;
+
+-- PREPARATION STEPS
+CREATE TABLE recipy.preparation_steps
+(
+    preparation_step_id character varying(255) NOT NULL,
+    description         character varying(255),
+    step_number         integer                NOT NULL,
+    recipe_id           character varying(255),
+    CONSTRAINT preparation_steps_pkey PRIMARY KEY (preparation_step_id),
+    CONSTRAINT preparation_steps_recipe_fkey FOREIGN KEY (recipe_id)
+        REFERENCES recipy.recipes (recipe_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+);
+
+ALTER TABLE recipy.preparation_steps
+    OWNER to root;
