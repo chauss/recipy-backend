343e4cbf1ed55b21088a06e75f12ef21ee05fec4
Christopher Hauß <christopher_hauss@gmx.de>
2023-04-02 15:36:11 +0200

    updated docker terraform provider

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
 output "db_ip_address" {
-  value       = docker_container.postgres.ip_address
+  value       = lookup(docker_container.postgres.network_data[0], "ip_address")
   description = "The IP address of the postgres database container."
 }
