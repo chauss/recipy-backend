770f1250e310a9f1c1ce74206c6f68f89d9ec63b
Christopher Hauß <christopher_hauss@gmx.de>
2023-06-01 22:55:29 +0200

    added creator to recipeOverview

--- src/main/kotlin/de/chauss/recipy/service/dtos/RecipeOverviewDto.kt
+++ src/main/kotlin/de/chauss/recipy/service/dtos/RecipeOverviewDto.kt
@@ -5,6 +5,7 @@ import de.chauss.recipy.database.models.Recipe
 data class RecipeOverviewDto(
     val recipeId: String,
     val name: String,
+    val creator: String,
     val created: Long,
 ) {
     companion object {
@@ -12,6 +13,7 @@ data class RecipeOverviewDto(
             return RecipeOverviewDto(
                 recipeId = recipe.recipeId,
                 name = recipe.name,
+                creator = recipe.creator,
                 created = recipe.created.toEpochMilli(),
             )
         }
